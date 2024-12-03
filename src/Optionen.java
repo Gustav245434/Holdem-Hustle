a8ff5c9be7bbb7bfb27eed97d0825bd2b2f203c7
Gustav245434 <gglatzel@student.tgm.ac.at>
2024-12-03 17:14:17 +0100

    Add Optionen

---
+++ src/Optionen.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Optionen extends JPanel {
+
+    public Optionen() {
+        super();
+
+        // Implement code for changing database connection, ...
+
+    }
+
+}
